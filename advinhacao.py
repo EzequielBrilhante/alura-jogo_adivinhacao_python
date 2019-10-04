2dd838557ca3bb628d16db0bbc1953ce0d42c009
Ezequiel Brilhante <ezequielrb7@gmail.com>
2019-10-04 00:20:58 -0300

    primeiro commit do jogo

---
+++ advinhacao.py
@@ -0,0 +1,29 @@
+print("********************************")
+print("Bem vindo ao jogo de Adivinhação")
+print("********************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+while(rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou " , chute_str)
+    chute = int(chute_str)
+
+    acertou = numero_secreto == chute
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! Chute maior que o número secreto")
+        elif(menor):
+            print("Você errou! Chute menor que o número secreto")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
